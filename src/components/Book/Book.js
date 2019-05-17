0dccebd50f3908f1ae369d4c2c1475b8c254e732
Ulukbek Tenizbaev <tenizbaevulukbek@outlook.com>
2019-05-17 14:25:14 +0600

    Create navigation

---
+++ src/components/Book/Book.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Book extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Book</h2>
+      </div>
+    );
+  }
+}
+
+export default Book;
